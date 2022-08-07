11f33e4e2256d4418a54451eb88efbeabcf3ea49
kamal1234567890 <kamaluddin1623@gmail.com>
2022-08-07 13:09:40 +0800

    first in github

--- app.mjs
+++ app.mjs
@@ -1,10 +1,10 @@
 import express from 'express'
-// import dotenv from 'dotenv'
+import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
 import { MakananRoute, UsersRoute, PenyakitRoute, PatientRoute } from './routes/index.mjs'
 import { getBobotUser } from './controllers/BobotController.mjs'
-// dotenv.config()
+dotenv.config()
 import * as databases from './utils/databases.mjs'
 
 const app = express()
@@ -28,6 +28,6 @@ app.use((req, res) => {
     res.send('<h1>404</h1>')
 })
 
-app.listen(process.env.PORT || 5000 , () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log('success')
 })
